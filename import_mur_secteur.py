442f61944750e02d515a91103d563f7b52981255
Matthias Kuhn <matthias@opengis.ch>
2016-09-27 16:37:06 +0200

    Create import script for mur_secteur

---
+++ import_mur_secteur.py
@@ -0,0 +1,20 @@
+import psycopg2
+import csv
+
+conn = psycopg2.connect("service=pg_avenches")
+cur = conn.cursor()
+
+with open('mur_secteur.csv', 'rb') as csvfile:
+    mur_secteurs = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+    # Skip header line
+    mur_secteurs = list(mur_secteurs)[1:]
+    for mur_secteur in mur_secteurs:
+        print(mur_secteur)
+        cur.execute("""
+            INSERT INTO site.mur_secteur (fk_mur, fk_secteur)
+            SELECT mur_id, secteur_id
+            FROM site.mur
+            LEFT JOIN site.secteur ON secteur_nom = %(secteur_nom)s
+            WHERE mur_nom = %(mur_nom)s;
+        """, {'mur_nom': mur_secteur[0], 'secteur_nom': mur_secteur[1]})
