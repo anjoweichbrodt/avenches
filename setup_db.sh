8b02ccfd8b98b0b0823db60722d97f0dea1f20df
Matthias Kuhn <matthias@opengis.ch>
2016-10-25 15:21:58 +0200

    Add data import to setup_db.sql

--- setup_db.sh
+++ setup_db.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 psql "service=pg_avenches" -v ON_ERROR_STOP=1 -f schema.sql
+psql "service=pg_avenches" -v ON_ERROR_STOP=1 -f monument.sql
+psql "service=pg_avenches" -v ON_ERROR_STOP=1 -f secteur.sql
+psql "service=pg_avenches" -v ON_ERROR_STOP=1 -f mur.sql
+python import_mur_secteur.py
