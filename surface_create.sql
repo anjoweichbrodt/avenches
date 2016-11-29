d1a5a78e228c21990c39a6d4d2519add30d5ef94
Matthias Kuhn <matthias@opengis.ch>
2016-11-29 12:37:18 +0100

    Use elevation level from existing surfaces during digitizing

--- surface_create.sql
+++ surface_create.sql
@@ -7,16 +7,30 @@ ST_ZMin(geom_3d) AS minimum_level,
 ST_ZMax(geom_3d)  AS maximum_level,
 exposition::site.expo,
 fk_mur,
-fk_secteur
+fk_secteur,
+NULL::text AS nomatlas
 FROM site.surface;
 
 CREATE OR REPLACE FUNCTION site.surface_create_insert()
   RETURNS trigger AS
 $BODY$
+DECLARE
+  import_surface RECORD;
+  minx INT;
+  maxx INT;
 BEGIN
+  SELECT
+  COALESCE(NEW.minimum_level, ST_YMIN(wkb_geometry)) AS minimum_level,
+  COALESCE(NEW.maximum_level, ST_YMAX(wkb_geometry)) AS maximum_level,
+  wkb_geometry
+  INTO import_surface
+  FROM import.surfaces
+  WHERE nomatlas = NEW.nomatlas;
+
+ 
   INSERT INTO site.surface (
            geom_3d -- create geometry
-           -- , geom_frontal
+           , geom_frontal
            , exposition
            , fk_mur
            , fk_secteur
@@ -24,13 +38,14 @@ BEGIN
      VALUES (
            ST_SetSRID(ST_MakePolygon(
              ST_MakeLine(ARRAY[
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), NEW.minimum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), NEW.minimum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), NEW.maximum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), NEW.maximum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), NEW.minimum_level)
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), import_surface.minimum_level),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), import_surface.minimum_level),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), import_surface.maximum_level),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), import_surface.maximum_level),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), import_surface.minimum_level)
              ])
            ), 2056)
+           , import_surface.wkb_geometry
            , NEW.exposition
            , NEW.fk_mur
            , NEW.fk_secteur
