3e8cb11f5aa31ee6ba89103d13377b5f1fb423e6
Matthias Kuhn <matthias@opengis.ch>
2016-11-29 11:55:10 +0100

    Add scripts to import surfaces to DB

--- setup_db.sh
+++ setup_db.sh
@@ -6,3 +6,4 @@ psql "service=pg_avenches" -v ON_ERROR_STOP=1 -f secteur.sql
 psql "service=pg_avenches" -v ON_ERROR_STOP=1 -f mur.sql
 psql "service=pg_avenches" -v ON_ERROR_STOP=1 -f surface_create.sql
 python import_mur_secteur.py
+ogr2ogr -f "PostgreSQL" PG:"service=pg_avenches dbname=pilot" -lco "SCHEMA=import" surfaces/surfaces.shp -nln surfaces
