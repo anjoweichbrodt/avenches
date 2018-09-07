63775a8c000e719abfcf918a8ae8258e1b26d1e2
Matthias Kuhn <matthias@opengis.ch>
2018-09-07 09:48:56 +0200

    Implement update and centimeter resolution for surface_create

--- surface_create.sql
+++ surface_create.sql
@@ -1,24 +1,88 @@
-﻿DROP VIEW IF EXISTS site.surface_create;
-CREATE VIEW site.surface_create AS
-SELECT
-surface_id,
-ST_MakeLine(ST_PointN(ST_ExteriorRing(geom_3d), 1), ST_PointN(ST_ExteriorRing(geom_3d), 2))::Geometry AS baseline,
-ST_ZMin(geom_3d) AS minimum_level,
-ST_ZMax(geom_3d)  AS maximum_level,
-exposition::site.expo,
-fk_mur,
-fk_secteur,
-NULL::text AS nomatlas
-FROM site.surface
-LEFT JOIN site.mur_part ON mur_part.mur_part_id = fk_mur_part;
+﻿DROP VIEW tra.surface_create;
 
-CREATE OR REPLACE FUNCTION site.surface_create_insert()
+CREATE OR REPLACE VIEW tra.surface_create AS 
+ SELECT surfaces.name,
+    st_makeline(st_pointn(st_exteriorring(surfaces.geom_3d), 1), st_pointn(st_exteriorring(surfaces.geom_3d), 2)) AS baseline,
+    st_zmin(surfaces.geom_3d::box3d)::numeric(10,2) AS minimum_level,
+    st_zmax(surfaces.geom_3d::box3d)::numeric(10,2) AS maximum_level,
+    surfaces.exposition,
+    wall_parts.wall::varchar,
+    wall_parts.sector::varchar,
+    NULL::smallint AS surface_part,
+    NULL::varchar AS atlas_name
+   FROM tra.surfaces
+   LEFT JOIN tra.wall_parts ON wall_parts.name = surfaces.wall_part;
+
+--------------------------------------------
+-- INSERT
+--------------------------------------------
+
+CREATE OR REPLACE FUNCTION tra.surface_create_insert()
   RETURNS trigger AS
 $BODY$
 DECLARE
   import_surface RECORD;
-  minx INT;
-  maxx INT;
+  minx numeric(10,2);
+  maxx numeric(10,2);
+BEGIN
+  SELECT
+  COALESCE(ST_YMIN(wkb_geometry), NEW.minimum_level) AS minimum_level,
+  COALESCE(ST_YMAX(wkb_geometry), NEW.maximum_level) AS maximum_level,
+  wkb_geometry	
+  INTO import_surface
+  FROM import.surfaces
+  WHERE nomatlas = NEW.atlas_name;
+
+ 
+  INSERT INTO tra.surfaces (
+           name,
+           geom_3d -- create geometry
+           , geom_frontal
+           , exposition
+           , wall_part
+           , surface_part
+           )
+     VALUES (
+           CONCAT(CONCAT(NEW.sector,'_' ,COALESCE(NEW.wall, split_part(NEW.atlas_name,'-' ,1))), '-', COALESCE(regexp_replace(split_part(split_part(NEW.atlas_name,'-' ,2), '_', 1), 'O', 'W')::tra.exposition, NEW.exposition), '-' || NULLIF(split_part(split_part(NEW.atlas_name, '-', 3), '_', 1), '')),
+           ST_SetSRID(ST_MakePolygon(
+             ST_MakeLine(ARRAY[
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), COALESCE(import_surface.minimum_level, NEW.minimum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), COALESCE(import_surface.minimum_level, NEW.minimum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), COALESCE(import_surface.maximum_level, NEW.maximum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), COALESCE(import_surface.maximum_level, NEW.maximum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), COALESCE(import_surface.minimum_level, NEW.minimum_level))
+             ])
+           ), 2056)
+           , ST_SetSRID(import_surface.wkb_geometry, 1)
+           , COALESCE(regexp_replace(split_part(split_part(NEW.atlas_name,'-' ,2), '_', 1), 'O', 'W')::tra.exposition, NEW.exposition)
+           , CONCAT(NEW.sector,'_' ,COALESCE(NEW.wall, split_part(NEW.atlas_name,'-' ,1)))
+           , COALESCE(NULLIF(split_part(split_part(NEW.atlas_name, '-', 3), '_', 1), '')::smallint, NEW.surface_part)
+           )
+           RETURNING name INTO NEW.name;
+     RETURN NEW;
+END; $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION tra.surface_create_insert()
+  OWNER TO qgis;
+x
+CREATE TRIGGER surface_create_on_insert
+  INSTEAD OF INSERT
+  ON tra.surface_create
+  FOR EACH ROW
+  EXECUTE PROCEDURE tra.surface_create_insert();
+
+--------------------------------------------
+-- UPDATE
+--------------------------------------------
+
+CREATE OR REPLACE FUNCTION tra.surface_create_update()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+  import_surface RECORD;
+  minx numeric(10,2);
+  maxx numeric(10,2);
 BEGIN
   SELECT
   COALESCE(ST_YMIN(wkb_geometry), NEW.minimum_level) AS minimum_level,
@@ -26,36 +90,33 @@ BEGIN
   wkb_geometry
   INTO import_surface
   FROM import.surfaces
-  WHERE nomatlas = NEW.nomatlas;
+  WHERE nomatlas = NEW.atlas_name;
 
  
-  INSERT INTO site.surface (
-           geom_3d -- create geometry
-           , geom_frontal
-           , exposition
-           , fk_mur_part
-           )
-     VALUES (
-           ST_SetSRID(ST_MakePolygon(
+  UPDATE tra.surfaces 
+     SET geom_3d =ST_SetSRID(ST_MakePolygon(
              ST_MakeLine(ARRAY[
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), import_surface.minimum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), import_surface.minimum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), import_surface.maximum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), import_surface.maximum_level),
-               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), import_surface.minimum_level)
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), COALESCE(import_surface.minimum_level, NEW.minimum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), COALESCE(import_surface.minimum_level, NEW.minimum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 2)), ST_Y(ST_PointN(NEW.baseline, 2)), COALESCE(import_surface.maximum_level, NEW.maximum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), COALESCE(import_surface.maximum_level, NEW.maximum_level)),
+               ST_MakePoint(ST_X(ST_PointN(NEW.baseline, 1)), ST_Y(ST_PointN(NEW.baseline, 1)), COALESCE(import_surface.minimum_level, NEW.minimum_level))
              ])
-           ), 2056)
-           , ST_SetSRID(import_surface.wkb_geometry, 1)
-           , NEW.exposition
-           , (SELECT mur_part_id FROM site.mur_part WHERE fk_mur = NEW.fk_mur AND fk_secteur = NEW.fk_secteur)
-           )
-           RETURNING surface_id INTO NEW.surface_id;
-     RETURN NEW;
+           ), 2056),
+         geom_frontal = ST_SetSRID(import_surface.wkb_geometry, 1),
+         exposition = COALESCE(regexp_replace(split_part(split_part(NEW.atlas_name,'-' ,2), '_', 1), 'O', 'W')::tra.exposition, NEW.exposition),
+         wall_part = CONCAT(NEW.sector,'_' ,COALESCE(NEW.wall, split_part(NEW.atlas_name,'-' ,1))),
+         surface_part = COALESCE(NULLIF(split_part(split_part(NEW.atlas_name, '-', 3), '_', 1), '')::smallint, NEW.surface_part)
+     WHERE name = OLD.name;
 END; $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
+ALTER FUNCTION tra.surface_create_update()
+  OWNER TO qgis;
 
--- DROP TRIGGER vw_access_aid_ON_INSERT ON qgep.access_aid;
 
-CREATE TRIGGER surface_create_ON_INSERT INSTEAD OF INSERT ON site.surface_create
-  FOR EACH ROW EXECUTE PROCEDURE site.surface_create_insert();
+CREATE TRIGGER surface_create_on_update
+  INSTEAD OF UPDATE
+  ON tra.surface_create
+  FOR EACH ROW
+  EXECUTE PROCEDURE tra.surface_create_update();
