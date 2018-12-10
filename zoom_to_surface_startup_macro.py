7738c331c3e2447786d54e1f6c623e56914e4fd5
Matthias Kuhn <matthias@opengis.ch>
2018-12-10 12:06:02 +0100

    Add zoom_to_surface startup macro

---
+++ zoom_to_surface_startup_macro.py
@@ -0,0 +1,39 @@
+# This startup script is designed to work with the Quick Attribution plugin
+# 
+#     https://github.com/opengisch/quick_attribution/
+#
+# If an attribute with the name 'surface' is changed on any layer via the quick
+# attribution plugin, the layer will zoom to the respective feature on the layer
+# "surfaces frontal"
+
+import qgis
+from qgis.core import QgsProject
+
+iface = qgis.utils.iface
+
+def openProject():
+    def zoomTo(name):
+        layer = QgsProject.instance().mapLayersByName('surfaces frontal')[0]
+        filter_expression = 'name = \'{name}\''.format(name=name)
+        feature = next(layer.getFeatures(filter_expression))
+        box = feature.geometry().boundingBox()
+        box = box.buffered(box.width()/10)
+        iface.mapCanvas().setExtent(box)
+        iface.mapCanvas().refresh()
+
+    def onCurrentValueChanged(field, value):
+        if field == 'surface':
+            zoomTo(value)
+
+    currentValueConnection = qgis.utils.plugins['quick_attribution'].dockWidget.currentValueChanged.connect(onCurrentValueChanged)
+    qgis.utils.plugins['quick_attribution'].__currentValueConnection = currentValueConnection
+
+
+def saveProject():
+    pass
+
+def closeProject():
+    currentValueConnection = qgis.utils.plugins['quick_attribution'].__currentValueConnection
+    if currentValueConnection:
+        qgis.utils.plugins['quick_attribution'].dockWidget.currentValueChanged.disconnect(currentValueConnection)
+
